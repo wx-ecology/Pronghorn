61ef1d1b06b30246cbaaaef91ec3f3d6d7cd90a6
Wenjing Xu <wenjing.xu@BERKELEY.EDU>
2019-10-27 18:05:25 -0700

    Oct27

---
+++ Script/Part8_Visualization_Straightness.R
@@ -0,0 +1,3 @@
+library(ggplot2)
+
+straightness <- read.csv(paste0("/Users/Mushy/Google Drive (wenjing.xu@berkeley.edu)/RESEARCH/Pronghorn/Analysis/FenceBehavior/DataTable/straightness_roll_10ind.csv"))
