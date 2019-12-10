e66fdec1924f739446314d2b127bb8508b9cb982
Wenjing Xu <wenjing.xu@BERKELEY.EDU>
2019-12-09 16:37:57 -0800

    fence distance sensitivity analysis

--- Script/03Part7_V2_BaBA_Official.R
+++ Script/03Part7_V2_BaBA_Official.R
@@ -39,7 +39,7 @@ interval <- 2 #define time interval (in hours) of the movement data
 ## Fence buffer distance in meters 
 # advised fence buffer distance is the 1st qu. of all points distance to fences
 # there is a seperate script that can calculate distance from all points to fences (Dist2FecneAnalysis_Official)
-FB.dist <- 50
+FB.dist <- 150
 
 ## tolerance parameter. If "a" is point in the buffer, "b" is a buffer outside of the buffer
 # x is the number of b that is allowed in between of a to allow the point series be considered as a continuous encounter event
@@ -108,7 +108,7 @@ fence.buffer <- raster::buffer(fence.sp, width=FB.dist)
 
 #read in movement data
 #ideally, the movement data should not have missing point. This trial file does have missing points.
-movement.df.all <- read.csv("Int2_Comp_Raw_All.csv") 
+movement.df.all <- read.csv("Int2_PRON_Raw_Final.csv") 
 movement.df.all$date <- as.POSIXct(strptime(as.character(movement.df.all$date),"%m/%d/%Y %H:%M")) #change the format based on the data
 movement.df.all <- movement.df.all <- movement.df.all[(!is.na(movement.df.all$date))&(!is.na(movement.df.all$Easting)),]
 
@@ -183,7 +183,7 @@ encounter.df = foreach (i = unique(movement.df.all$Location.ID),
   encounter.df <- encounter.df[which(!is.na(encounter.df$burst)),]  #all points that are in buffer in one dataframe
   encounter.df
 }
-write.csv(encounter.df, file = ("I2_All_FB50_B4_P36_EncounterEvents.csv"))
+write.csv(encounter.df, file = ("I2_All_FB150_B4_P36_EncounterEvents.csv"))
 
 close(pb)
 #stop cluster
@@ -284,7 +284,7 @@ event.df = foreach (
 }
 #event.df.temp <- event.df
 #event.df <- event.df.temp
-write.csv(event.df, file = "I2_All_FB50_B4_P36_Step1Cls.csv")
+write.csv(event.df, file = "I2_All_FB150_B4_P36_Step1Cls.csv")
 
 close(pb)
 #stop cluster
@@ -335,5 +335,5 @@ for (i in 1:nrow(event.df)) {
   }
 }
 event.df.1 <- event.df
-write.csv(event.df.1,"I2_All_FB50_B4_P36_FinalCls.csv")
+write.csv(event.df.1,"I2_All_FB150_B4_P36_FinalCls.csv")
 
